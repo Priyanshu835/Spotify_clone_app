14a9b3c411dceb75a737033df42d2623434dd139
priyanshu kumar rai <priyanshurai835@gmail.com>
2021-12-10 21:01:05 +0530

    interface get start by player and slidebar components

--- src/App.js
+++ src/App.js
@@ -4,31 +4,38 @@ import Login from "./Login";
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
+import {useDataLayerValue} from "./DataLayer";
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react';
 
 const spotify=new SpotifyWebApi();
 
 function App() {
-const [token,setToken]=useState(null);
+const [{user,token},dispatch]=useDataLayerValue();
 
   useEffect(()=>{
     const hash=getTokenFromUrl();
     window.location.hash="";
     const _token=hash.access_token;
     if(_token){
-      setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
       spotify.setAccessToken(_token);
       spotify.getMe().then(user =>{
-        console.log(user)
+        dispatch({
+          type: 'SET_USER',
+          user: user,
+        })
       })
     }
-    console.log('I have a token >>>',token);
   },[]);
 
   return (
     <div className="app">
       {
         token?(
-          <Player/>
+          <Player spotify={spotify}/>
           ):(
           <Login/>
         )
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,7 @@
       playlists:[],
       playing:false,
       item:null,
+      token: 'BQDMenc1XFN_I7lbM9uyoxEym4XlS90EOkFKW2AmLfZcMA3CixgnahdBvZJCesXgU7tr1ouWl1TZBXeUriyFHimoUUPAnU6m5Q8YRj8p9ZClBp3AeIpRr_-W72T5b-Bmw2JZK0Fa6XKJ2qCMERIYMxOLxrbw-Aixya8EBxV25siWPa5T',
  }
  export const reducer=(state,action)=>{
 console.log(action);
@@ -11,6 +12,11 @@ switch(action.type){
         return{
             ...state,
             user:action.user
+        }
+    case 'SET_TOKEN':
+        return{
+            ...state,
+            token:action.token,
         }
         default:
             return state;
